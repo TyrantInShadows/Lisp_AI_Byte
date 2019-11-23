c8080c5abc4b8e197f1a2cb26aded65ebea86342
AnubisLP <lazarp@elfak.rs>
2019-11-23 16:37:13 +0100

    Moj Deo

--- Byte.lisp
+++ Byte.lisp
@@ -1,10 +1,9 @@
-(setq prom'(B(K1 K2)))
-
 (defstruct gameState
             naPotezu 
             prviIgra
             tabla
             velicinaTable
+            popunjenaPolja
             rezultatIgre
             krajIgre 
  )
@@ -22,6 +21,10 @@
     (GenerisiTabelu (gameState-velicinaTable state) 0)
     (princ "Unesite P ili K u zavisnosti da li zelite igrac ili racunar da igra prvi, respektivno: ")
     (setf (gameState-prviIgra state) (read))
+
+    (setf (gameState-tabla state) (PronadjiElement+ 1 2 (gameState-tabla state) 0 t "dodaj" 'X))
+    (setf (gameState-tabla state) (PronadjiElement+ 1 2 (gameState-tabla state) 0 t "dodaj" 'O))
+    (setf (gameState-tabla state) (PronadjiElement+ 1 1 (gameState-tabla state) 0 t "izbaci" 'X))
 )
 (defun GenerisiTabelu(velicina i)
     (cond
@@ -33,8 +36,8 @@
 (defun GenerisiRed(i j)
     (cond
         ((equalp j 8)'())
-        ((equalp 0 (mod (+ i j) 2)) (cons (list 'B (OdrediDisk i)) (GenerisiPolje i (1+ j))))
-        (t (cons (list 'W '()) (GenerisiPolje i (1+ j))))
+        ((equalp 0 (mod (+ i j) 2)) (cons (list 'B (OdrediDisk i)) (GenerisiRed i (1+ j))))
+        (t (cons (list 'W '()) (GenerisiRed i (1+ j))))
     )
 )
 
@@ -47,18 +50,119 @@
 )
 
 (defun PronadjiElement (i j l)
-    (ProndjiElement1 j (ProndjiElement1  i l 0) 0)
+    (PronadjiElement1 j (PronadjiElement1  i l 0) 0)
 )
 
-(defun ProndjiElement1 (i l index)
+(defun PronadjiElement+ (i j l index bool operacija el)
+    (cond 
+        ((null l)'())
+        ((equalp i index)(if (equalp bool t)
+                        (cons (PronadjiElement+ j i (car l) 0 '() operacija el) (cdr l))
+                        (if (equalp operacija "dodaj") 
+                            (cons (cons (car (car l)) (list (DodajElement (car (cdr (car l))) el))) (cdr l))
+                            (cons (cons (car (car l))(list (IzbaciElement (car (cdr (car l))) (length (car (cdr (car l)))) 0))) (cdr l))  
+                        )))
+        (t(cons (car l) (PronadjiElement+ i j (cdr l) (1+ index) bool operacija el)))
+    )
+)
+(defun PronadjiElement1 (i l index)
     (cond 
         ((null l)'())
         ((equalp i index)(car l))
-        (t(ProndjiElement1 i (cdr l) (1+ index)))
+        (t(PronadjiElement1 i (cdr l) (1+ index)))
     )
 )
 
-(defun VisinaSteka (i j l)
-    (length (car (cdr (PronadjiElement i j l))))
+(defun VisinaSteka (l)
+    (length (car (cdr l)))
 )
-(defun test)
+
+(defun DodajElement (l el)
+    (cond
+        ((null l)(list el))
+        (t(cons (car l) (DodajElement (cdr l) el)))
+    )
+)
+
+(defun IzbaciElement (l duzina index)
+    (cond
+        ((null l)'())
+        ((equalp (- 1 duzina) index)'())
+        (t(cons (car l) (IzbaciElement (cdr l) duzina (1+ index))))
+    )
+)
+
+(defun test(l)
+    (car (cdr (car l)))
+)
+
+(defun LegitimnoPomeranjeNaPrazno (iDiska jDiska iPomeraj jPomeraj)
+    (setf (OdrediNajblizi iDiska jDiska (gameState-velicinaTable state) (gameState-tabla state) 0) lista)
+    (cond
+        ((AND(< (- iPomeraj iDiska) 0)(< (- jPomeraj jDiska) 0)) (ProveriKavadrant iPomeraj jPomeraj 1 lista))
+        ((AND(> (- iPomeraj iDiska) 0)(> (- jPomeraj jDiska) 0)) (ProveriKavadrant iPomeraj jPomeraj 2 lista))
+        ((AND(< (- iPomeraj iDiska) 0)(> (- jPomeraj jDiska) 0)) (ProveriKavadrant iPomeraj jPomeraj 3 lista))
+        ((AND(> (- iPomeraj iDiska) 0)(< (- jPomeraj jDiska) 0)) (ProveriKavadrant iPomeraj jPomeraj 4 lista))
+    )
+)
+
+(defun ProveriKavadrant (iPomeraj jPomeraj kon l)
+    (cond
+        ((null l)'())
+        ((equalp kon 1) (if (>= (+ iPomeraj jPomeraj) (+ (car (car (cdr (car l)))) (cdr (car (cdr (car l)))) ))
+                            (t)
+                            (ProveriKavadrant iPomeraj jPomeraj kon (cdr l))
+                        ))
+        ((equalp kon 2) (if (<= (+ iPomeraj jPomeraj) (+ (car (car (cdr (car l)))) (cdr (car (cdr (car l)))) ))
+                            (t)
+                            (ProveriKavadrant iPomeraj jPomeraj kon (cdr l))
+                        ))
+        ((equalp kon 3) (if (>= (- iPomeraj jPomeraj) (- (car (car (cdr (car l)))) (cdr (car (cdr (car l)))) ))
+                            (t)
+                            (ProveriKavadrant iPomeraj jPomeraj kon (cdr l))
+                        ))
+        ((equalp kon 4) (if (<= (- iPomeraj jPomeraj) (- (car (car (cdr (car l)))) (cdr (car (cdr (car l)))) ))
+                            (t)
+                            (ProveriKavadrant iPomeraj jPomeraj kon (cdr l))
+                        ))
+    )
+)
+
+(defun OdrediNajblizi (iDiska jDiska maxi l index)
+    (append (gameState-popunjenaPolja state) (ParLista (- iDiska index) (+ iDiska index) l maxi iDiska jDiska))
+    (setf (OdrediNajmanjiNivo index  (gameState-popunjenaPolja state)) lista)
+    (cond
+        ((null lista) (OdrediNajblizi iDiska jDiska maxi l (1+ index)))
+        (t(lista))
+    )
+)
+
+(defun ObradiListu (i j l iDiska jDiska)
+    (cond
+        ((null l)'())
+        ((> (VisinaSteka (car l)) 0)(if (AND (equalp i iDiska) (equalp j jDiska))   
+                                    (ObradiListu i (1+ j) (cdr l) iDiska jDiska)
+                                    (if (> (abs (- i iDiska)) (abs (- j jDiska)))
+                                        (cons (cons (- i iDiska) (list 'i 'j)) (ObradiListu i (1+ j) (cdr l) iDiska jDiska))
+                                        (cons (cons (- j jDiska) (list 'i 'j))(ObradiListu i (1+ j) (cdr l) iDiska jDiska))
+                                    )))
+        (t(ObradiListu i (1+ j) (cdr l) iDiska jDiska))
+    )
+)
+
+(defun OdrediNajmanjiNivo (index l)
+    (cond
+        ((null l)'())
+        ((equalp (car (car l)) index)(cons (car l) (OdrediNajmanjiNivo index (cdr l))))
+        (t(OdrediNajmanjiNivo index (cdr l)))
+    )
+)
+
+(defun ParLista (i1 i2 l maxi iDiska jDiska)
+    (case
+    (equalp i1 i2) (ObradiListu i1 0 (PronadjiElement1 i1 l 0) iDiska jDiska)
+    ((OR (< i1 0) (> i1 maxi))(ObradiListu i2 0 (PronadjiElement1 i2 l 0) iDiska jDiska))
+    ((OR (< i2 0) (> i2 maxi))(ObradiListu i1 0 (PronadjiElement1 i1 l 0) iDiska jDiska))
+    (t(append (ObradiListu i1 0 (PronadjiElement1 i1 l 0) iDiska jDiska) (ObradiListu i2 0 (PronadjiElement1 i2 l 0) iDiska jDiska)))
+    )
+)
